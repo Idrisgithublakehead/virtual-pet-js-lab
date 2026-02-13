238574b17c6a36d0d394e67c20a218ae60ef9f6a
Idris Zahir <izahir@lakeheadu.ca>
2026-02-13 00:53:37 -0500

    feat: define sentence part arrays and initialize index variables

---
+++ storymaker/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+/* ok so first i set use script which is a built in script function which allows me to now
+be more strict with mistakes and Makes your code safer and cleaner */
+
+
+/* ok now lets move on to actually creating the array for the const objects*/
+
+// ok so these are all the random Sentence parts in arrays which we can grab later
+const subjects = ["A pirate", "The squirrel", "My neighbor", "A ninja", "The penguin"];
+const actions = ["jumped over", "dropped", "painted", "chased", "accidentally kicked"];
+const descriptors = ["a glowing", "a sticky", "a mysterious", "a tiny", "a massive"];
+const objects = ["pumpkin", "backpack", "remote control", "sandwich", "keyboard"];
+const locations = ["at the beach", "inside the mall", "on a rooftop", "in the forest", "under the table"];
+
+/* now we can actually build the tRACKERDS, OF COURSE NOW
+IT IS set to 0 which will be updated later */
+
+let i1 = 0;
+let i2 = 0;
+let i3 = 0;
+let i4 = 0;
+let i5 = 0;
