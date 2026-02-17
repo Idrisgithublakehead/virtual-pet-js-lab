b37f83aed1b63dd937edacc0ea0092e204326e90
Idris Zahir <izahir@lakeheadu.ca>
2026-02-16 21:45:22 -0500

    feat: implement RGB background update with helper functions

--- lab-2/lab-2/lab-2/js/page-color-changer.js
+++ lab-2/lab-2/lab-2/js/page-color-changer.js
@@ -8,3 +8,42 @@ const blueSlider = document.getElementById("blue");
 
 // Select the body element
 const pageBody = document.body;
+
+
+
+
+// function that builds the rgb color string
+function buildRGBColor() {
+
+    let r = redSlider.value;
+    let g = greenSlider.value;
+    let b = blueSlider.value;
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+// function that applies the color to the page
+function applyBackgroundColor() {
+
+    let rgbColor = buildRGBColor();
+    pageBody.style.backgroundColor = rgbColor;
+}
+
+
+// ok so as soon as tghe page loads we can run this background color once when the page loads
+applyBackgroundColor();
+
+
+// each slider will have its own  event listener
+redSlider.addEventListener("input", function () {
+    applyBackgroundColor();
+});
+
+greenSlider.addEventListener("input", function () {
+    applyBackgroundColor();
+});
+
+blueSlider.addEventListener("input", function () {
+    applyBackgroundColor();
+});
