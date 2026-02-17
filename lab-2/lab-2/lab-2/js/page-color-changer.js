4b594d54012e4b8cf57acd15bdd1188e211f8814
Idris Zahir <izahir@lakeheadu.ca>
2026-02-16 21:38:03 -0500

    feat: select RGB sliders and body element

---
+++ lab-2/lab-2/lab-2/js/page-color-changer.js
@@ -0,0 +1,10 @@
+// idris zahir lab 2 java script
+//so first we need to grab the sliders in the html file
+// we grab red green blue and sent a const pagebody
+
+const redSlider = document.getElementById("red");
+const greenSlider = document.getElementById("green");
+const blueSlider = document.getElementById("blue");
+
+// Select the body element
+const pageBody = document.body;
